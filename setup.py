1164a521c1959f18f3c25aa052ec5b3b7dbc15d8
Cold Sun <bikingrox@gmail.com>
2019-02-08 15:36:33 -0500

    3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'Where to download it.',
     'author_email': 'bikingrox@gmail.com',
     'version': '0.1',
-    'install_requires': ['pytest'],
+    'install_requires': ['pytest', 'pygame'],
     'packages': ['game'],
     'scripts': [],
     'name': 'game'
