d5b5bca58ed700196c835c39d42d80f994079e58
Cold Sun <bikingrox@gmail.com>
2019-02-08 15:03:21 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'pygame',
+    'author': 'Cold Sun',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'bikingrox@gmail.com',
+    'version': '0.1',
+    'install_requires': ['pytest'],
+    'packages': ['game'],
+    'scripts': [],
+    'name': 'game'
+}
+
+setup(**config)
